edc404d06c26e9381674f09559fd1283bf082a93
sgnb221@gmail.com <sgnb221@gmail.com@14657092-81e6-a9cf-a3a4-659e33ff7e88>
2011-02-07 19:26:07 +0000

    Updating! UI in progress.

---
+++ trunk/src/com/vroom/Monitor.java
@@ -0,0 +1,13 @@
+package com.vroom;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Monitor extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.monitor);
+	}
+}
