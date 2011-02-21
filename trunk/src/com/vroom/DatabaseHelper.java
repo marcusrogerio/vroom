b91c0baa994e85c85fb06e6ea7e446d1216477ed
sgnb221@gmail.com <sgnb221@gmail.com@14657092-81e6-a9cf-a3a4-659e33ff7e88>
2011-02-21 22:49:22 +0000

    Complete - task : Add updateHistory Function

--- trunk/src/com/vroom/DatabaseHelper.java
+++ trunk/src/com/vroom/DatabaseHelper.java
@@ -9,6 +9,8 @@ import static com.vroom.Constants.TABLE_USERHISTORY;
 import static com.vroom.Constants.historyId;
 import static com.vroom.Constants.voltage;
 import static com.vroom.Constants.temperature;
+import static com.vroom.Constants.troubleCode;
+import static com.vroom.Constants.rpm;
 import static com.vroom.Constants.timestamp;
 
 import static com.vroom.Constants.TABLE_USERVEHICLES;
@@ -20,6 +22,8 @@ import static android.provider.BaseColumns._ID;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
 
 
 /**
@@ -30,6 +34,7 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class DatabaseHelper extends SQLiteOpenHelper {
 	
+    	private static final String TAG = "DatabaseHelper";
 	private static final String DATABASE_NAME = "vroomInfo.db";
 	private static final int DATABASE_VERSION = 1;
 
@@ -42,6 +47,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	 * @version 1, 2/14/2011
 	 * 
 	 * @param context The context in which we're working. Type Context. 
+	 * 
+	 * @throws none
 	 */
     DatabaseHelper (Context context){
     	super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -54,6 +61,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
      * @version 1, 2011
      * 
      * @param db The database we're working on. Type SQLite Database
+     * 
+     * @throws none
      */
     @Override
     public void onCreate(SQLiteDatabase db){
@@ -66,9 +75,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     	//Create the USERHISTORY table
     	db.execSQL("CREATE TABLE " + TABLE_USERHISTORY + " ("
     			+ _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-    			+ historyId + " INTEGER, "
+    			+ historyId + " TEXT, "
     			+ voltage + " DECIMAL(4), "
-    			+ temperature + " DECIMAL(4), "
+    			+ temperature + " INTEGER, "
+    			+ rpm + " INTEGER, "
+    			+ troubleCode + " TEXT, "
     			+ timestamp + " STRFTIME);");
     	
     	//Create the USERVEHICLES table
@@ -88,10 +99,57 @@ public class DatabaseHelper extends SQLiteOpenHelper {
      * @param db the database we're working on. Type SQLiteDatabase.
      * @param oldVersion the previous version number of the database. Type int.
      * @param newVersion the new version number of the database. Type int.
+     * 
+     * @throws none
      */
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
     	db.execSQL("DROP DATABASE IF EXISTS " + DATABASE_NAME);
     	onCreate(db);
+    }
+    
+    /**
+     * UpdateHistory updates one row of the history table. 
+     * 
+     * The UpdateHistory function is designed to take any number of arguments and update a history row as needed. 
+     * Any passed values that are not in use must be given a null value. 
+     * 
+     * @author Neale Petrillo
+     * @version 1, 2/21/2011
+     * 
+     * @param db The SQLite database we're working with. Type SQLiteDatabase
+     * @param vehicleId The id of the current vehicle. Type String
+     * @param tempe The coolant temperature to be added to the vehicle history. Type int
+     * @param revs The current RPM to be added to the vehicle history. Type int
+     * @param volt The current voltage to be added to the vehicle history. Type float
+     * @param code The current trouble code to be added to the vehicle history. Type Byte
+     * 
+     * @return true on success, false on failure
+     * @throws none
+     */
+    public Boolean updateHistory (SQLiteDatabase db, String vehicleId, int tempe, int revs, float volt, byte code){
+	
+	try{
+	    //Build the sql string
+	    String sql = "INSERT INTO " + TABLE_USERHISTORY 
+			+ " (" + historyId + ","
+			+ voltage + ","
+			+ temperature + ","
+			+ rpm + ","
+			+ troubleCode + ") VALUES ("
+			+ vehicleId + ","
+			+ volt + ","
+			+ tempe + ","
+			+ revs + ","
+			+ code + ");";
+	    
+	    //Execute the sql
+	    db.execSQL(sql);
+	    
+	    return true;
+	}catch (Exception e){
+	    Log.e(TAG, "Unable to update history via updateHistory. "+e.getMessage(), e.getCause());
+	    return false;
+	}
     }
 }
